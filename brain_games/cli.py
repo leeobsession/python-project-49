a056ad65821b5c003fa27b9de19323ce7d8bb788
leeobsession <lera03skor@gmail.com>
2023-05-31 04:48:32 +1000

    add flake8

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,5 +1,8 @@
 import prompt
 
+
 def welcom_user():
-    name = prompt.string('May I have your name?','')
+
+    name = prompt.string('May I have your name?')
+
     return f"Hello, {name}!"
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 from brain_games.cli import welcom_user
+
+
 def main():
     print('Welcome to the Brain Games!')
     print(welcom_user())
