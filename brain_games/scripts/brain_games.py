80b569e4527e13e4f83c0651058bf69e421c88f6
leeobsession <lera03skor@gmail.com>
2023-05-31 04:19:11 +1000

    add cli.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+from brain_games.cli import welcom_user
 def main():
     print('Welcome to the Brain Games!')
+    print(welcom_user())
     if __name__ == "__main__":
         main()
 
